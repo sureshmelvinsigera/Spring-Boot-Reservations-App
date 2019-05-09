dd92a1ade69bca2034ebb6c61fd75387e6524188
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2019-05-09 16:41:03 -0400

    Initial commit

---
+++ src/main/java/com/bestofpython/com/ReservationsApplication.java
@@ -0,0 +1,14 @@
+package com.bestofpython.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationsApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ReservationsApplication.class, args);
+    }
+
+}
